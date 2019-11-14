b98ea8c1c3bfe48403789ef515bc5140c8262aa5
Talaa Zina <talazina@gmail.com>
2019-11-14 13:56:43 +0100

    init project

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+  render() {
+  	return (
+  		 <nav>
+            <h1>navigation bar!</h1>
+         </nav>
+  		);
+  }
+
+}
+
+export default Header;
