fd388c031a7655cd2e772b487425a82c8c3c8fa1
Talaa Zina <talazina@gmail.com>
2019-11-18 22:04:07 +0100

    Updates

---
+++ precache-manifest.4ca38fc6c2987e47c1bdd55f0ab492fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89ac3b22ed76c435af2bb44d88b312b",
+    "url": "/zina-portfolio/index.html"
+  },
+  {
+    "revision": "f4b192ad2d2beb068c9c",
+    "url": "/zina-portfolio/static/css/main.406ffb0e.chunk.css"
+  },
+  {
+    "revision": "8376e91e1356f8466c5a",
+    "url": "/zina-portfolio/static/js/2.e489d29f.chunk.js"
+  },
+  {
+    "revision": "f4b192ad2d2beb068c9c",
+    "url": "/zina-portfolio/static/js/main.966b4043.chunk.js"
+  },
+  {
+    "revision": "c0249f8cd4feb4c6bb9e",
+    "url": "/zina-portfolio/static/js/runtime-main.9e8c6bdd.js"
+  },
+  {
+    "revision": "971f16e96f243ca10f35480d21a58a8a",
+    "url": "/zina-portfolio/static/media/designecologist.971f16e9.jpg"
+  },
+  {
+    "revision": "2edff82045c87266af58a341058ae2da",
+    "url": "/zina-portfolio/static/media/food.2edff820.jpg"
+  },
+  {
+    "revision": "e5047b8a5052a36a55a8340347da00e2",
+    "url": "/zina-portfolio/static/media/reading-about-code-on-hackernoon.e5047b8a.jpg"
+  }
+]);
