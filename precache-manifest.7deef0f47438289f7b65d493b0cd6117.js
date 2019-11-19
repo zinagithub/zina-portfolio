d22a21fad3f683443a0462f5bd548ea437f39952
Talaa Zina <talazina@gmail.com>
2019-11-19 10:31:53 +0100

    Updates

---
+++ precache-manifest.7deef0f47438289f7b65d493b0cd6117.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3cd5a1221ec78dd0f631092b75344e",
+    "url": "/zina-portfolio/index.html"
+  },
+  {
+    "revision": "a5dae0966156054a5722",
+    "url": "/zina-portfolio/static/css/main.406ffb0e.chunk.css"
+  },
+  {
+    "revision": "8376e91e1356f8466c5a",
+    "url": "/zina-portfolio/static/js/2.e489d29f.chunk.js"
+  },
+  {
+    "revision": "a5dae0966156054a5722",
+    "url": "/zina-portfolio/static/js/main.e6f25703.chunk.js"
+  },
+  {
+    "revision": "c0249f8cd4feb4c6bb9e",
+    "url": "/zina-portfolio/static/js/runtime-main.9e8c6bdd.js"
+  },
+  {
+    "revision": "971f16e96f243ca10f35480d21a58a8a",
+    "url": "/zina-portfolio/static/media/designecologist.971f16e9.jpg"
+  },
+  {
+    "revision": "2edff82045c87266af58a341058ae2da",
+    "url": "/zina-portfolio/static/media/food.2edff820.jpg"
+  },
+  {
+    "revision": "e5047b8a5052a36a55a8340347da00e2",
+    "url": "/zina-portfolio/static/media/reading-about-code-on-hackernoon.e5047b8a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.cce9049d0e1cf2a4683136ebee7da304.js"
+  "/zina-portfolio/precache-manifest.7deef0f47438289f7b65d493b0cd6117.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zina-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
